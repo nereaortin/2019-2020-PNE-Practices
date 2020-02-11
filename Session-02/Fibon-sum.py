36550b42374d3d261a469cf2f22ca846aea4fc1c
nortin <n.ortin.2019@alumnos.urjc.es>
2020-02-11 15:26:26 +0100

    my first commit

--- Session-02/Fibon-sum.py
+++ Session-02/Fibon-sum.py
@@ -1,18 +1,19 @@
-def Fibon(n):
-    if n < 0:
-        print("Incorrect input")
-        # First Fibonacci number is 0
-    elif n == 1:
-        return 0
-    # Second Fibonacci number is 1
-    elif n == 2:
-        return 1
-    else:
-        return Fibon(n - 1) + Fibon(n - 2)
-def fibosum(n):
-    fibonacci = 0
-    for i in range(1, n+1):
-        fibonacci = fibonacci + fibon(i)
-    return fibonacci
-print("The sum of the first", 5, "terms of the fibonacci series:", fibosum(5))
-print("The sum of the first", 10, "terms of the Fibonacci seies:", fibosum(10))
+def fibo(n):
+    index1= 0
+    index2= 1
+    sumn = 0
+    count = 0
+
+    for number in range(0, n+1):
+        if number == 0:
+            sumn = 0
+        elif number == 1:
+            sumn = sumn + 1
+        else:
+            count = index1 + index2
+            index1 = index2
+            index2 = count
+            sumn = sumn + count
+    return sumn
+print("The sum of the first", 5, "terms of the fibonacci series:", fibo(5))
+print("The sum of the first", 10, "terms of the Fibonacci seies:", fibo(10))

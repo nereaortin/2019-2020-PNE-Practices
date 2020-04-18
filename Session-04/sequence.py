b78f1fbc74f15d64f31780d5bbc7aaa03ec6384b
nereaortin <nerea.ortin@gmail.com>
2020-04-18 18:34:58 +0200

    done

--- Session-04/sequence.py
+++ Session-04/sequence.py
@@ -1,11 +1,12 @@
+#--Session 4. Exercise 5
+
 from pathlib import Path
 
-try:
-    FILENAME = "ADA.txt"
-    file_contents = Path(FILENAME).read_text()
-    content = file_contents.split("\n")[1:]
-    Body_file = ",".join(content).replace(",", "")
-    print("Body of the U5.txt file:")
-    print(len(Body_file))
-except FileNotFoundError:
-    print("That file does not exist")
+FILENAME = "ADA.txt"
+file_contents = Path(FILENAME).read_text()
+data = file_contents.split('\n')
+
+body = data[1:]
+text = "".join(body)
+
+print(len(body))

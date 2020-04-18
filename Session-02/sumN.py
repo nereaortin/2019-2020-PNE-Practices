07aa880b40b0058b0cf4527447bf06800eb43339
nereaortin <nerea.ortin@gmail.com>
2020-04-18 17:15:35 +0200

    done

--- Session-02/sumN.py
+++ Session-02/sumN.py
@@ -1,15 +1,11 @@
 # -- 1 + 2 + 3 + .... + 20
-
 #-- 1 + ...... + 100
 
 
 def sumn(n):
-    count = 0
     res = 0
-    n1 = 0
-    n2 = 1
     for i in range(1, n+1):
-        
+        res += i
     return res
 
 

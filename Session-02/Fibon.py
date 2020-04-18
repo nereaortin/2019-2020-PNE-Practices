97c3d1adbdc0a4b9472dd007de1a84d566dcb8b3
nereaortin <nerea.ortin@gmail.com>
2020-04-18 17:07:06 +0200

    done

--- Session-02/Fibon.py
+++ Session-02/Fibon.py
@@ -2,21 +2,22 @@
 
 def fibonacci(n):
     index1 = 0
-    index2 = 0
-    sumn = 0
+    index2 = 1
+
     count = 0
     for number in range(0, n+1):
         if number == 0:
-            sumn = 0
+            count = 0
         # First Fibonacci number is 0
         elif number == 1:
-            sumn = sumn + 1
+            count = count + 1
     # Second Fibonacci number is 1
         else:
             count = index1 + index2
             index1 = index2
-            sumn = sumn + count
-    return sumn
+            index2 = count
+    return count
 
-print("sumn of the first 5 terms of trhe fibonacci series is :", fibonacci(5))
-print("sumn of the first 5 terms of trhe fibonacci series is :", fibonacci(5))
+print("5th Fibonacci term: ", fibonacci(5))
+print("10th Fibonacci term:", fibonacci(10))
+print("15th Fibonacci term:", fibonacci(15))

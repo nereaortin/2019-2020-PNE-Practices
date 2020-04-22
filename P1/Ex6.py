669ceb43358db679ab09bc29ed4a358cd9632e2a
nereaortin <nerea.ortin@gmail.com>
2020-04-23 00:30:03 +0200

    done!

---
+++ P1/Ex6.py
@@ -0,0 +1,18 @@
+#Session 7.Exercise 6
+
+from Seq1 import Seq  # Seq is the class
+
+print("-----| Practice 1, Exercise 6 |------")
+# we are creating sequences passing a string with the bases to an object
+# null sequence
+s1 = Seq()
+
+# -- Create a valid sequence
+s2 = Seq("ACTGA")
+
+# -- Create an invalid sequence
+s3 = Seq("Invalid sequence")
+
+for i, s in enumerate([s1, s2, s3]):
+    print("Sequence", i, ": (Lenght:", s.len(), ")", s)
+    print("  Bases:", s.count())
---
+++ P1/Ex9.py
@@ -0,0 +1,20 @@
+#Session 7. Exercise 9
+
+from Seq1 import Seq  # Seq is the class
+
+print("-----| Practice 1, Exercise 9 |------")
+# we are creating a sequence from files in fasta format
+
+FOLDER = "../Session-04/"
+EXT = ".txt"
+GENES = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
+# -- Create a Null sequence
+s = Seq()
+
+# -- Initialize the null seq with the given file in fasta format
+s.seq_read_fasta(FOLDER + GENES[0] + EXT)
+
+print("Sequence: ", " (Lenght:", s.len(), ")", s)
+print("      Bases:", s.count())
+print("      Rev: ", s.seq_reverse())
+print("      Comp: ", s.seq_complement())

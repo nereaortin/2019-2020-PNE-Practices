a4c275a1d8cf8a8fe036fe27d1edada2c0364d7c
nereaortin <nerea.ortin@gmail.com>
2020-04-22 23:40:30 +0200

    done!

--- Session-06/Ex1.py
+++ Session-06/Ex1.py
@@ -1,28 +1,27 @@
+#Session 6.Exercise 1
 class Seq:
     def __init__(self, strbases):
         self.strbases = strbases
         # passed as argument when creating the object
-        print("New sequence created!")
-        # It is a special method that is called every time a new object is created
-
+        print("New sequence created!")#method called every time a new object is created
         bases = ['A', 'C', 'G', 'T']
 
-        for base in strbases:
-            if base not in bases:
+        for letter in strbases:
+            if letter not in bases:
                 print("ERROR!!")
-                print("Sequence 2: ERROR!!")
+                self.strbases = "ERROR"
             return
+        self.strbases = strbases
 
     def __str__(self):
-        # it run each time something is print
-        return self.strbases
-        # it gives the internal info of the object
+        return self.strbases# shows the internal info of the object
 
     def len(self):
-        # self means inside the method
+        #calculate the length of the sequence
         return len(self.strbases)
 
 
 s1 = Seq("ACCTGC")
 s2 = Seq("Hello? Am I a valid sequence?")
 print(f"Sequence 1: {s1}")
+print(f"Sequence 2: {s2}")

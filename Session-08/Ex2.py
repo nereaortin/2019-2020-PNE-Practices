f04a99791b1356f7465cef06514a900cbb2165bc
nereaortin <nerea.ortin@gmail.com>
2020-04-24 00:08:50 +0200

    done!

---
+++ Session-08/Ex2.py
@@ -0,0 +1,8 @@
+Destination IP 	   Name (if known) 	   Time
+212.168.111.134 	Self 	         0.023 ms, 0.045 ms...
+212.128.255.129 	f-l3208-pc01     3.35 ms, 3.83 ms
+8.8.8.8 	         Google          3.81 ms, 3.65 ms
+10.7.48.87 	        Mobile           43.3 ms, 68.2 ms
+212.128.240.50      www.urjc.es      2.46 ms , 2.29 ms
+140.82.118.3        github.com       36.2 ms , 36.3 ms
+208.80.154.224)     wikipedia.org    133 ms

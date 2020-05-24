56450b54d6fbaca43993854e0e10f1707bfd5c4d
nereaortin <nerea.ortin@gmail.com>
2020-05-24 21:38:51 +0200

    checked!

---
+++ Final-Project/client.py
@@ -0,0 +1,31 @@
+import http.client
+
+PORT = 8080
+SERVER = 'localhost'
+
+print(f"\nConnecting to server: {SERVER}:{PORT}\n")
+
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER, PORT)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+    conn.request("GET", "/listSpecies?limit=10&json=1")
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode("utf-8")
+
+# Print the information in the object
+print("CONTENT: ")
+print(data1)
